9c533871c46244fee38df0565fbbbac84a2106d4
taeminlee25 <108789989+taeminlee25@users.noreply.github.com>
2025-09-21 15:51:55 +0900

    initial commit

---
+++ web-app-backend/src/test/java/com/example/web_app_backend/WebAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
